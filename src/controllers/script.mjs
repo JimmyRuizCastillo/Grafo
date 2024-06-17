9231ff5b298c2d3c7d433a240875b05bf70a0a58
JimmyRuizCastillo <233288@ids.upchiapas.edu.mx>
2024-06-17 09:50:29 -0600

    commit dfs, css, complete

--- src/controllers/script.mjs
+++ src/controllers/script.mjs
@@ -1,23 +1,25 @@
 import Graph from "../models/Graph.mjs";
-let g = new Graph(8)
+let g = new Graph(6)
 let comboOrigin = document.getElementById("comboCityOrigin")
-let comboDestiny = document.getElementById("comboCityDestiny")
+let comboCityAddDestiny = document.getElementById("comboCityAddDestiny")
 let btnCheck = document.getElementById("btnCheck")
+let btnValidateAdd = document.getElementById("btnValidateAdd")
 
-g.addVertices("Tuxtla","Cintalapa","Tonala","Arriaga","Chiapa de Corzo","Suchiapa","VillaFlores", "San Cristobal")
+g.addVertices("Polleria","Farmacia","Ferreteria","Zapateria","Licoreria","Floreria")
 
-g.addConexion("Tuxtla","Cintalapa")
-g.addConexion("Tuxtla","San Cristobal")
-g.addConexion("Tuxtla","Arriaga",8)
-g.addConexion("Tuxtla","Chiapa de Corzo",11)
-g.addConexion("Arriaga","Tonala",9)
-g.addConexion("Cintalapa","Arriaga",10)
+g.addConexion("Polleria","Zapateria",5)
+g.addConexion("Polleria","Farmacia",3)
+g.addConexion("Farmacia","Licoreria",2)
+g.addConexion("Farmacia", "Zapateria",7)
+g.addConexion("Zapateria","Ferreteria",11)
+g.addConexion("Ferreteria", "Floreria",6)
 
 
 fillComboBox(comboOrigin)
-fillComboBox(comboDestiny)
+fillComboBox(comboCityAddDestiny)
 
 function fillComboBox(comboBox){
+    comboBox.innerHTML=""
     g.getMap().keys().forEach((key) => {
         let option = document.createElement('option')
         option.textContent = key
@@ -26,12 +28,39 @@ function fillComboBox(comboBox){
 }
 
 btnCheck.addEventListener("click",()=>{
-    console.log(g.dfs(comboOrigin.ariaValueMax, comboDestiny.value))
+    let rutas = []
+    let showRute = document.getElementById("showRutes")
+    showRute.innerHTML=""
+    addRutes(rutas)
+    rutas.forEach((value)=>{
+        let txtRute = document.createElement("h3")
+        txtRute.textContent = "Ruta: " + value
+        showRute.appendChild(txtRute)
+    })
+})
+
+function addRutes(rutas){
+    g.dfs(comboOrigin.value,(a)=>{
+        rutas.push(a)
+    })
+}
+
+btnValidateAdd.addEventListener("click",()=>{
+    let newLocal = document.getElementById("inputAddLocal").value
+    let endLocal = document.getElementById("comboCityAddDestiny").value
+    let distanceLocal = document.getElementById("inputAddDistancia").value
+    g.addV(newLocal)
+    g.addConexion(newLocal,endLocal,distanceLocal)
+    fillComboBox(comboOrigin)
+    fillComboBox(comboCityAddDestiny)
+    console.log(g.getMap())
+
+    console.log(g.getList())
 })
 
 
 //g.dfs('Tuxtla')
 
-//console.log(g.getMap())
+console.log(g.getMap())
 
-//console.log(g.getList())
+console.log(g.getList())
--- src/models/Graph.mjs
+++ src/models/Graph.mjs
@@ -4,83 +4,81 @@ export default class Graph {
     #listaAdyacencia = []
     #map = new Map()
 
-    constructor() {}
+    constructor() { }
 
     addVertices(...vertices) {
         for (let value of vertices) {
             this.#listaAdyacencia.push(new LinkedList())
-            this.#map.set(value,this.#listaAdyacencia.length-1)
+            this.#map.set(value, this.#listaAdyacencia.length - 1)
         }
     }
 
     addV(value) {
         this.#listaAdyacencia.push(new LinkedList())
-        this.#map.set(value,this.#listaAdyacencia.length-1)
+        this.#map.set(value, this.#listaAdyacencia.length - 1)
     }
 
-    addConexion(start, end, weight=1){
+    addConexion(start, end, weight = 1) {
         if (this.#map.has(start) && this.#map.has(end)) {
-            this.#listaAdyacencia[this.#map.get(start)].add(new Vertex(end,weight))
-            this.#listaAdyacencia[this.#map.get(end)].add(new Vertex(start,weight))
+            this.#listaAdyacencia[this.#map.get(start)].add(new Vertex(end, weight))
+            this.#listaAdyacencia[this.#map.get(end)].add(new Vertex(start, weight))
             return true
         }
         return false;
     }
 
-    dfs(start,end) {
-        let visited = []
-        let stack = []
-      
-        visited.push(start)
-        stack.push(start)
-      
-        while (stack.length > 0) {
-          let current = stack.pop()
-          let index = this.#map.get(current)
-          let currentNode = this.#listaAdyacencia[index].head
-          if(current == end){
-            return;
-          }
-          while (currentNode) {
-            if (!visited.includes(currentNode.value.value)) {
-              visited.push(currentNode.value.value)
-              stack.push(currentNode.value.value)
-            }
-            currentNode = currentNode.next
-          }
-        }
+    dfs(start, callback) {
+        let visited = [];
+        this.dfsRecursive(start, callback, visited);
       }
-    
-    bfs(callback){
+      
+      dfsRecursive(current, callback, visited) {
+        visited.push(current);
+        callback(current);
+      
+        let index = this.#map.get(current);
+        let currentNode = this.#listaAdyacencia[index].head;
+      
+        while (currentNode) {
+          if (!visited.includes(currentNode.value.value)) {
+            this.dfsRecursive(currentNode.value.value, callback, visited);
+          }
+          currentNode = currentNode.next;
+        }
+      
+        visited.pop();
+      }      
+
+    bfs(callback) {
         let queue = []
         let list = []
         const entries = [...structuredClone(this.#map)]
-        for (let i=0; i<this.#listaAdyacencia.length;i++)
+        for (let i = 0; i < this.#listaAdyacencia.length; i++)
             list[i] = false
-        
+
         let [value] = entries[0]
         queue.push(value)
-        
+
         while (queue.length > 0) {
             let val = queue.shift() //Sacamos el primer elemento de la cola
             callback(val) //Imprimimos el valor
             list[this.#map.get(val)] = true //Marcamos de visitado
-            for (let i=0;i<this.#listaAdyacencia[this.#map.get(val)].length;i++) {
-                if (this.#listaAdyacencia[this.#map.get(val)][i]){
+            for (let i = 0; i < this.#listaAdyacencia[this.#map.get(val)].length; i++) {
+                if (this.#listaAdyacencia[this.#map.get(val)][i]) {
                     let [key] = entries[i]
-                    if (!list[this.#map.get(key)] && !queue.includes(key)) 
+                    if (!list[this.#map.get(key)] && !queue.includes(key))
                         queue.push(key) //Agregamos los vecinos a la cola
                 }
             }
         }
 
     }
-    
-    getMap(){
+
+    getMap() {
         return this.#map
     }
 
-    getList(){
+    getList() {
         return this.#listaAdyacencia
     }
 }
